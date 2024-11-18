7a12f785223d6c5cf8e12b5927a985963fcb95ae
sleepingcyber <sakhi16425@gmail.com>
2024-11-18 13:36:04 -0600

    Reg version CyPatWindowsAutomated.ps1
    Testing Registry edits

--- CyPatWindowsAutomated.ps1
+++ CyPatWindowsAutomated.ps1
@@ -1,43 +1,13 @@
-﻿#variables
-$TempFilePath = "$env:Temp\secedit.inf"
-$LogFilePath = "$env:Temp\secedit.log"
+#password policies reg path
+$PassowrdPolicyPath = "HKLM:\SYSTEM\CurrentControlSet\Services\Netlogon\Parameters"
+$AccountPolicyPath = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon"
 
-#exporting the current sec policy
-Write-Output "Exporting current sec policy"
-secedit /export /cfg $TempFilePath /quiet
-
-#read exported policy
-Write-Output "Reading exported pol"
-$SecurityPolicy = Get-Content $TempFilePath
-
-
-#adding policies
-
-#password Policies
-$SecurityPolicy = $SecurityPolicy -replace '^PasswordHistorySize=.*', 'PasswordHistorySize = 5'
-$SecurityPolicy = $SecurityPolicy -replace '^MaximumHistorySize=.*', 'PasswordHistorySize = 90'
-$SecurityPolicy = $SecurityPolicy -replace '^MinimumPasswordAge=.*', 'MinimumPasswordAge = 10'
-$SecurityPolicy = $SecurityPolicy -replace '^MinimumPasswordLength=.*', 'MinimumPasswordLength= 5'
-$SecurityPolicy = $SecurityPolicy -replace '^PasswordComplexity=.*', 'PasswordComplexity = 1'
-$SecurityPolicy = $SecurityPolicy -replace '^ClearTextPassword=.*', 'ClearTextPassword = 0'
-Write-Output "Password policies updated successfully!"
-#Account lockout policies
-$SecurityPolicy = $SecurityPolicy -replace '^LockoutBadCount=.*', 'LockoutBadCount = 5'
-$SecurityPolicy = $SecurityPolicy -replace '^LockoutDuration=.*', 'LockoutDuration = 30'
-$SecurityPolicy = $SecurityPolicy -replace '^ResetLockoutCount=.*', 'ResetLockoutCount = 30'
-Write-Output "Account lockout policies updated successfully!"
-
-#saving updated
-Write-Output "Saving updated policy"
-$SecurityPolicy | Set-Content -Path $TempFilePath -Force
-
-#aplying updated policy
-Write-Output "applying updated policy"
-secedit /configure /db/ secedit.sdb /cfg $TempFilePath /overwrite /log $LogFilePath /quiet
+#password policies
+Set-ItemProperty -Path $PasswordPolicyPath -Name "PasswordHistorySize" -Value 5
+Set-ItemProperty -Path $PasswordPolicyPath -Name "MaximumPasswordAge" -Value 90
+Set-ItemProperty -Path $PasswordPolicyPath -Name "MinimumPasswordAge" -Value 10
+Set-ItemProperty -Path $PasswordPolicyPath -Name "MinimumPasswordLength" -Value 5
+Set-ItemProperty -Path $PasswordPolicyPath -Name "PasswordComplexity" -Value 1
+Set-ItemProperty -Path $PasswordPolicyPath -Name "ClearTextPassword" -Value 0
 
 
-
-# Clean up temporary files
-Write-Output "Cleaning up temporary files..."
-Remove-Item -Path $TempFilePath -Force
-Remove-Item -Path $LogFilePath -Force
