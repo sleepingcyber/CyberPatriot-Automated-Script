4b9f508f64144b8aa684cf8c83c181601398b82c
sleepingcyber <sakhi16425@gmail.com>
2024-11-18 23:33:58 -0600

    Update CyPatWindowsAutomated.ps1
    added stackoverflow-found script, TODO: test and implement more security policies.

--- CyPatWindowsAutomated.ps1
+++ CyPatWindowsAutomated.ps1
@@ -10,4 +10,45 @@ Set-ItemProperty -Path $PasswordPolicyPath -Name "MinimumPasswordLength" -Value 
 Set-ItemProperty -Path $PasswordPolicyPath -Name "PasswordComplexity" -Value 1
 Set-ItemProperty -Path $PasswordPolicyPath -Name "ClearTextPassword" -Value 0
 
+#from:https://stackoverflow.com/questions/55774478/enforce-password-complexity-on-windows-using-powershell
+#ToDO: Implement this into script to make local sec policy changes
+Function Parse-SecPol($CfgFile){ 
+    secedit /export /cfg "$CfgFile" | out-null
+    $obj = New-Object psobject
+    $index = 0
+    $contents = Get-Content $CfgFile -raw
+    [regex]::Matches($contents,"(?<=\[)(.*)(?=\])") | %{
+        $title = $_
+        [regex]::Matches($contents,"(?<=\]).*?((?=\[)|(\Z))", [System.Text.RegularExpressions.RegexOptions]::Singleline)[$index] | %{
+            $section = new-object psobject
+            $_.value -split "\r\n" | ?{$_.length -gt 0} | %{
+                $value = [regex]::Match($_,"(?<=\=).*").value
+                $name = [regex]::Match($_,".*(?=\=)").value
+                $section | add-member -MemberType NoteProperty -Name $name.tostring().trim() -Value $value.tostring().trim() -ErrorAction SilentlyContinue | out-null
+            }
+            $obj | Add-Member -MemberType NoteProperty -Name $title -Value $section
+        }
+        $index += 1
+    }
+    return $obj
+}
 
+Function Set-SecPol($Object, $CfgFile){
+   $SecPool.psobject.Properties.GetEnumerator() | %{
+        "[$($_.Name)]"
+        $_.Value | %{
+            $_.psobject.Properties.GetEnumerator() | %{
+                "$($_.Name)=$($_.Value)"
+            }
+        }
+    } | out-file $CfgFile -ErrorAction Stop
+    secedit /configure /db c:\windows\security\local.sdb /cfg "$CfgFile" /areas SECURITYPOLICY
+}
+
+
+$SecPool = Parse-SecPol -CfgFile C:\test\Test.cgf
+$SecPool.'System Access'.PasswordComplexity = 1
+$SecPool.'System Access'.MinimumPasswordLength = 8
+$SecPool.'System Access'.MaximumPasswordAge = 60
+
+Set-SecPol -Object $SecPool -CfgFile C:\Test\Test.cfg
