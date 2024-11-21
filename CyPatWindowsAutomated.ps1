713fc27d51d197276de1c37751a226d422390665
sleepingcyber <sakhi16425@gmail.com>
2024-11-21 13:03:58 -0600

    Add files via upload

--- CyPatWindowsAutomated.ps1
+++ CyPatWindowsAutomated.ps1
@@ -377,11 +377,13 @@ $Settings = @(
         Name = "AllowPKU2UAuthentication"; # Network Security: Allow PKU2U authentication requests to use online identities
         Value = 0 # Disabled
     },
+    <#
     @{
         KeyPath = "HKLM:\SYSTEM\CurrentControlSet\Control\Lsa";
         Name = "KerberosEncryptionTypes"; # Network security: Configure encryption types allowed for Kerberos
         Value = "AES128_HMAC_SHA1, AES256_HMAC_SHA1, Future encryption types"
     },
+    #>
     @{
         KeyPath = "HKLM:\SYSTEM\CurrentControlSet\Control\Lsa";
         Name = "DoNotStoreLANManagerHash"; # Network security: Do not store LAN Manager hash value on next password change
@@ -391,6 +393,7 @@ $Settings = @(
         Name = "LDAPClientSigningRequirements"; # Network security: LDAP client signing requirements
         Value = 1 # Negotiate signing
     },
+    <#
     @{
         KeyPath = "HKLM:\SYSTEM\CurrentControlSet\Control\Lsa";
         Name = "MinimumSessionSecurityNTLMSSPClients"; # Network security: Minimum session security for NTLM SSP clients
@@ -401,36 +404,7 @@ $Settings = @(
         Name = "MinimumSessionSecurityNTLMSSPServers"; # Network security: Minimum session security for NTLM SSP servers
         Value = "Require NTLMv2 session security, Require 128-bit encryption"
     },
-    @{
-        KeyPath = "HKLM:\SYSTEM\CurrentControlSet\Control\Lsa";
-        Name = "RestrictNTLMAuditIncomingNTLM"; # Network security: Restrict NTLM: Audit Incoming NTLM Traffic
-        Value = 1 # Enable auditing for all accounts
-    },
-    @{
-        KeyPath = "HKLM:\SYSTEM\CurrentControlSet\Control\Lsa";
-        Name = "RestrictNTLMAuditNTLMAuthentication"; # Network security: Restrict NTLM: Audit NTLM authentication in this domain
-        Value = 2 # Enable all (DC only)
-    },
-    @{
-        KeyPath = "HKLM:\SYSTEM\CurrentControlSet\Control\Lsa";
-        Name = "RestrictNTLMAuditOutgoingNTLM"; # Network security: Restrict NTLM: Outgoing NTLM traffic to remote servers
-        Value = 2 # Audit all or higher
-    },
-    @{
-        KeyPath = "HKLM:\SYSTEM\CurrentControlSet\Control\Lsa";
-        Name = "LDAPClientSigningRequirements"; # Network security: LDAP client signing requirements
-        Value = 1 # Negotiate signing
-    },
-    @{
-        KeyPath = "HKLM:\SYSTEM\CurrentControlSet\Control\Lsa";
-        Name = "MinimumSessionSecurityNTLMSSPClients"; # Network security: Minimum session security for NTLM SSP clients
-        Value = "Require NTLMv2 session security, Require 128-bit encryption"
-    },
-    @{
-        KeyPath = "HKLM:\SYSTEM\CurrentControlSet\Control\Lsa";
-        Name = "MinimumSessionSecurityNTLMSSPServers"; # Network security: Minimum session security for NTLM SSP servers
-        Value = "Require NTLMv2 session security, Require 128-bit encryption"
-    },
+    #>
     @{
         KeyPath = "HKLM:\SYSTEM\CurrentControlSet\Control\Lsa";
         Name = "RestrictNTLMAuditIncomingNTLM"; # Network security: Restrict NTLM: Audit Incoming NTLM Traffic
